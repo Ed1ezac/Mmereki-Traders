8b6b17c12be249904aed06a4d9fbf203beeb2e82
Ed1ezac <ed1ezac@gmail.com>
2021-10-04 15:23:02 +0200

    adds vue js components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import Hello from './components/hello-vue.vue';
+
+const app = createApp({
+    components:{
+        Hello,
+    }
+});
+app.mount("#app");
