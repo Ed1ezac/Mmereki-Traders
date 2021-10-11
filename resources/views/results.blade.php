00b68a44263c9d012155b201ce3ec64b5ae7166e
letsodon <letsomosinyi93@gmail.com>
2021-10-11 17:25:27 +0200

    results work in progress

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -2,8 +2,21 @@
 
 @section('content')
 
-<div class="mt-32 mx-16 p-8 bg-yellow-500">
-    
+<div class="flex flex-col mt-2 mx-16 p-8 bg-white shadow">
+    <div class='flex flex-row mt-2 mx-16 bg-red-200 shadow'>
+        <div class=' w-3/4 mt-2 mb-10 ml-4 p-2 bg-blue-500 shadow'>bj sons</div>
+        <div class=' w-1/4 mt-2 ml-4 p-2 bg-blue-500 shadow'>company logo</div>
+    </div>
+    <div class=' flex space-x-4  mx-16 p-2 bg-blue-200 shadow'>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>Register Guranteed</div>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>Telephone</div>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>Mob</div>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>email</div>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>sms</div>
+        <div class='flex-1 p-1 bg-blue-500 shadow'>call back</div>
+    </div>
+   
+        
 </div>
 
 @endsection
