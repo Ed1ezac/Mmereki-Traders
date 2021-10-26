64f1e4a431241dc21eb51b48236ed2172bbe03f4
Ed1ezac <ed1ezac@gmail.com>
2021-10-26 17:19:13 +0200

    Adds support for Setswana language

---
+++ resources/lang/tsw/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Email le password tse o di tsentseng ga se tsone.',
+    'password' => 'Password e o tsentseng ga se yone.',
+    'throttle' => 'O lekile gantsi.O tla leka gape morago ga :seconds seconds.',
+
+];
