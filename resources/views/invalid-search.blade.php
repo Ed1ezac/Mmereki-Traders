8b9e750d79a6a0a7c1fac6e79885b10085ab3ffc
letsodon <letsomosinyi93@gmail.com>
2021-10-26 17:17:03 +0200

    today's progress

--- resources/views/invalid-search.blade.php
+++ resources/views/invalid-search.blade.php
@@ -2,73 +2,37 @@
 
 @section('content')
 <section>
-    <div class='bg-white '>
-        <p class='bg-gray-300 font-bold text-black text-center p-8'>Invalid Search</p>
+    <div class='bg-gray-100'>
+        <p class='bg-gray-200 font-bold text-blue-900 text-2xl text-center p-8 shadow'>
+            Invalid Search
+        </p>
         <div class=' bg-gray-200 justify-between flex flex-row'>
-            <p class=' bg-gray-200 w-18 p-8 '>The quick brown fox  lorem ipsum dolor...</p>
-            <div class=' bg-gray-300 right-center mr-8'> Traders Directory</div>
+            <p class='flex flex-center bg-gray-200 w-18 p-8 '>
+                The quick brown fox  lorem ipsum dolor...
+            </p>
+            
         </div>
-        <div class=' bg-white  shadow'>
-            <svg class="h-20 w-20 ml-15 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>
-            <p class=' bg-gray-200 p-10 text-center'>The quick brown fox ipsum dolor....</p>
+        <div class='flex flex-col bg-gray-100  shadow'>
+            <div class='bg-gray-100 mx-auto '>
+                <svg xmlns="http://www.w3.org/2000/svg" class=" text-red-500 h-20 w-20 text-center" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+            </div class=' bg-gray-100 '>
+                <div class='bg-gray-100 mx-auto'>
+                     <p class=' bg-gray-100 p-10 ml-24 text-red-500 font-bold text-xl text-center'>
+                    Invalid Search! Try again.
+                    </p>
+                </div>
+            </div>
+            <div>
+            <div class='mx-auto  bg-gray-100 text-lg text-blue-900 p-5 mt-2 mb-2 w-52'>
+                 Search Again
+            </div>
 
         </div>
     </div>
 
 </section>
-<section>
-    <div class='mt-3 p-2 ml-16 bg-white shadow'>
-        
-        <div class="flex flex-row justify-around">
-            <div class='flex flex-col'>
-                <p class='bg-gray-200 top-1/4 my-2'>text.....</p>
-                <div class=' bg-gray-200  '>see how it works </div>
-                
-            </div>
-            <div class='flex flex-col'>
-                <h2 class='capitalize font-bold text-gray-900  tracking-widest'>Useful links</h2>
-                <div class=' bg-gray-200 mt-3'>About us</div>
-                <div class='mt-3 bg-gray-200'>Help and Advice</div>
-                <div class=' mt-3 bg-gray-200'>News</div>
-                <div class='mt-3 bg-gray-200'>Customer gurantee</div>
-            </div>
-            <div class='flex flex-col'>
-                <div class=' bg-gray-200'>Trade associations</div>
-                <div class=' bg-gray-200 mt-3'>Trade directory</div>
-                <div class=' bg-gray-200 mt-3'>Payment service</div>
-            </div>
-            <div class='flex flex-col'>
-                <div class='bg-gray-200'>Privacy policy</div>
-                <div class='bg-gray-200 mt-3'>Disclaimer</div>
-                <div class='bg-gray-200 mt-3 flex flex-row'> Social pages</div>
-            </div>
 
-            <div class='flex flex-col'>
-                <div class='bg-gray-200'>Adversing</div>
-                <div class='bg-gray-200 mt-3'>Contact us</div>
-                <div class='bg-gray-200 mt-3'>Terms </div>
-            </div>
-        </div>
-
-        </div>
-         <div class='mt-3 ml-16 bg-white'>
-             <div class="flex justify-between ...">
-                 <div class="bg-gray-200 order-last flex flex-row "><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>app store</div>
-                 <div class='bg-gray-200 mt-3 flex flex-row'><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>social media platform</div>
-                 <div class='bg-gray-200 mt-3 flex flex-row'><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>google play</div>
-                </div> 
-         </div>
-        <div class='mt-3 ml-16 bg-white'></div>
-    </div>
-</section>
 @endsection
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -2,35 +2,51 @@
 
 @section('content')
 
-<div class="flex flex-col rounded mt-16 mx-16 bg-white shadow">
-    <div class='flex flex-row  bg-white shadow'>
-        <div class=' w-3/4 mt-2 ml-4 p-2 bg-gray-200 shadow'>
+  <div class="flex flex-col mx-auto max-w-4xl rounded mt-16 md:flex bg-white shadow-lg">
+    <div class='flex flex-row justify-between  bg-white shadow'>
+        <div class='w-full pr-6 pl-4 pt-4 p-2 bg-gray-200 shadow'>
             <a href="/trader-details"><h2 class="capitalize font-bold text-blue-900  tracking-widest">bj sons</h2></a>
-            <p class="my-2">The quick brown fox  lorem ipsum dolor...</p>
+            <p class=" pb-5 pt-4 mt-4">The quick brown fox  lorem ipsum dolor...</p>
         </div>
 
-        <div class=' w-1/4 ml-4 p-2 bg-gray-200 shadow'><svg class="h-20 w-20 center text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg></div>
+        <div class='border-blue-900 border-solid border-2  w-40 bg-gray-200 shadow md:flex-shrink-0'>
+          <svg xmlns="http://www.w3.org/2000/svg" class=" text-blue-900 h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+        </div>
     </div>
-    <div class=' flex space-x-4 p-2 shadow'>
-        <div class='flex-1 p-1 bg-gray-200 flex flex-row'><svg class="h-7 w-7 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>Register Guranteed</div>
-        <div class='flex-1 p-1  bg-gray-200'>Telephone</div>
-        <div class='flex-1 p-1 flex flex-row bg-gray-200'>Mob</div>
-        <div class='flex-1 p-1 flex flex-row bg-gray-200 '><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>Email</div>
-        <div class='flex-1 p-1 flex flex-row bg-gray-200'><svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>Sms</div>
-        <div class='flex-1 p-1 flex flex-row bg-gray-200'><svg class="h-5 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>Call Back</div>
+      <div class=' text-sm flex space-x-4 p-2 shadow'>
+        <div class='border-blue-900 border-solid border-2  font-bold flex-1 p-1 bg-gray-200 flex flex-row'>
+          <svg xmlns="http://www.w3.org/2000/svg" class=" text-blue-900 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+            Register Guranteed
+        </div>
+        
+      
+        
+        <div class=' text-sm font-bold flex-1 p-1 border-blue-900 border-solid border-2  flex flex-row bg-gray-200'>
+               <svg xmlns="http://www.w3.org/2000/svg" class=" text-blue-900 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+              </svg> 
+              +267 498752
+            </div>
+            <div class=' text-sm font-bold flex-1 p-1 border-blue-900 border-solid border-2  flex flex-row bg-gray-200'>
+              <svg xmlns="http://www.w3.org/2000/svg" class="  text-blue-900 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+              </svg>
+              +267 76025479
+            </div>
+            <div class=' border-blue-900 border-solid border-2 text-sm font-bold flex-1 p-1  flex flex-row bg-gray-200 '>
+              <svg xmlns="http://www.w3.org/2000/svg" class=" text-blue-900 blue- h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>  
+              letsomosinyi@gmail.com
+            </div>
     </div>
    
         
-</div>
+  </div>
 
 @endsection
