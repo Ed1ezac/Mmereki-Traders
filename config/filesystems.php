348880a27de52d8665e7a7e606c9da25c1ec431f
Ed1ezac <ed1ezac@gmail.com>
2022-06-20 08:44:34 +0200

    minor fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,8 +68,8 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('images') => storage_path('app/images'),
-        public_path('images') => storage_path('app/docs'),
+        public_path('images') => storage_path('app/public/images'),
+        public_path('docs') => storage_path('app/docs'),
     ],
 
 ];
