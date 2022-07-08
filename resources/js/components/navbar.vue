88a4c339e087141908f3bcf6fc896e52362aa29b
Ed1ezac <ed1ezac@gmail.com>
2022-07-08 06:34:49 +0200

    adds avatar and menu popup

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -5,7 +5,6 @@
             <div class="flex">
                 <a href="/" class="self-center">
                     <img class="h-9 w-12 bg-green-600 rounded sm:inline-block" alt="logo">
-                    <!--img class="h-9 sm:hidden" :src="logoUri" alt="logo"-->
                 </a>
 
                 <div class="hidden md:flex md:ml-12 md:space-x-12">
@@ -24,11 +23,6 @@
                     <a v-if="isAuth" href="/membership" v-bind:class="currentUrl == 'membership' ? 'text-white border-b-4 border-white hover:text-gray-800':'text-gray-400 hover:text-white py-1'" class="my-navbar-link">
                         <div class="text-sm font-bold">
                             Membership
-                        </div>
-                    </a>
-                    <a v-if="isAuth" href="/settings" v-bind:class="currentUrl == 'settings' ? 'text-white border-b-4 border-white hover:text-gray-800':'text-gray-400 hover:text-white py-1'" class="my-navbar-link">
-                        <div class="text-sm font-bold">
-                            Settings
                         </div>
                     </a>
                     <a v-if="isAuth" href="/admin/companies" v-bind:class="currentUrl == 'companies' ? 'text-white border-b-4 border-white hover:text-gray-800':'text-gray-400 hover:text-white py-1'" class="my-navbar-link">
@@ -50,7 +44,40 @@
             </div>
 
             <div class="hidden md:flex md:items-center pt-0 pb-0 px-2">
-                <a v-if="isAuth" href="/logout" v-on:click.prevent="logout" class="block px-2 py-1 sm:ml-2 rounded text-white hover:no-underline hover:bg-gray-400 border-transparent focus:ring-offset-0 focus:ring-gray-300">Logout</a>
+                <!---Avatar---->
+                <div v-if="isAuth" class="relative inline-block text-left">
+                    <Menu>
+                        <MenuButton type="button" class="bg-gray-800 relative z-10 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-gray-300" id="user-menu" aria-expanded="false" aria-haspopup="true">
+                            <span class="sr-only">Open user menu</span>
+                            <div class="w-8 h-8 bg-gray-700 rounded-full overflow-hidden shadow-inner text-center bg-purple table">
+                                <span class="table-cell text-sm text-white font-bold align-middle">MT</span>
+                            </div>
+                        </MenuButton>
+                        <transition
+                            enter-active-class="transition duration-100 ease-out"
+                            enter-from-class="transform scale-95 opacity-0"
+                            enter-to-class="transform scale-100 opacity-100"
+                            leave-active-class="transition duration-75 ease-out"
+                            leave-from-class="transform scale-100 opacity-100"
+                            leave-to-class="transform scale-95 opacity-0">
+                            <MenuItems class="absolute right-0 w-52 mt-2 origin-top-right bg-white border border-gray-200 rounded-md shadow-lg outline-none">
+                                 <MenuItem>
+                                    <div class="flex-col border-b py-1 bg-gray-50 border-gray-100">
+                                        <h3 class="whitespace-normal px-4 w-52 text-gray-800">Toto Kelapile</h3>
+                                        <h3 class="whitespace-normal px-4 text-sm w-52 text-gray-400">Toto Pty Ltd</h3>
+                                        <h3 class="whitespace-normal px-4 text-xs w-52 text-gray-400">toto@gmail.com</h3>
+                                    </div>
+                                </MenuItem>
+                                 <MenuItem v-slot="{ active }">
+                                    <a href="/settings" :class="active ? 'bg-gray-100 text-gray-900':'text-gray-700'" class="block px-4 py-3 text-sm text-gray-700 hover:bg-gray-100 hover:no-underline" role="menuitem">Settings</a>
+                                </MenuItem> 
+                                <MenuItem v-slot="{ active }">
+                                    <a v-on:click.prevent="logout" href="/logout" :class="active ? 'bg-gray-100 text-gray-900':'text-gray-700'" class="block px-4 py-3 rounded-b-md text-sm text-gray-700 hover:bg-gray-100 hover:no-underline" role="menuitem">Logout</a>
+                                </MenuItem>
+                            </MenuItems>
+                        </transition>
+                    </Menu>
+                </div>
                 <!---->
                 <a v-if="!isAuth" href="/login" class="block text-white rounded-sm border border-white px-2 py-1 hover:no-underline hover:bg-gray-400 border-transparent focus:ring-offset-0 focus:ring-gray-300">Login</a>
                 <a v-if="!isAuth" href="/register" class="block sm:ml-2 text-white rounded-sm border border-white px-2 py-1 border-transparent hover:no-underline hover:bg-gray-400 focus:ring-offset-0 focus:ring-gray-300">Register</a>
@@ -58,7 +85,7 @@
         </div>
 
         <div :class="isOpen ? 'block': 'hidden'" class="md:hidden overflow-auto px-4 pt-1 pb-4">
-            <a v-if="isAuth" href="/home" v-bind:class="currentUrl == 'home' ? 'text-white bg-gray-900 border-white hover:text-gray-400':'text-gray-400 hover:text-white'" class="rounded my-navbar-link-mobile">
+            <a v-if="isAuth" href="/home" v-bind:class="currentUrl == 'home' ? 'text-white bg-gray-900 border-white hover:text-gray-400':'text-gray-400 hover:text-white hover:bg-gray-900'" class="rounded my-navbar-link-mobile">
                 <div class="text-sm font-bold">
                     Dashboard
                 </div>
@@ -73,18 +100,34 @@
                     Membership
                 </div>
             </a>
-            <a v-if="isAuth" href="/settings" v-bind:class="currentUrl == 'settings' ? 'text-white bg-gray-900 hover:text-gray-400':'text-gray-400 hover:text-white hover:bg-gray-900'" class="rounded my-navbar-link-mobile">
-                <div class="text-sm font-bold">
-                    Settings
-                </div>
-            </a>
             <a v-if="isAuth" href="/admin/companies" v-bind:class="currentUrl == 'companies' ? 'text-white bg-gray-900 hover:text-gray-400':'text-gray-400 hover:text-white hover:bg-gray-900'" class="rounded my-navbar-link-mobile">
                 <div class="text-sm font-bold">
                     Traders
                 </div>
             </a>
-            <div class="border border-t border-gray-50 my-1"></div>
-            <a v-if="isAuth" href="/logout" v-on:click.prevent="logout" class="block px-2 py-1 sm:ml-2 rounded text-white hover:no-underline hover:bg-gray-400 border-transparent focus:ring-offset-0 focus:ring-gray-300">Logout</a>
+            <div v-if="isAuth" class="border-b border-gray-50 my-1"></div>
+            <!---Avatar And All---->
+            <div>
+                <div v-if="isAuth" class="flex items-center py-1">
+                    <div class="w-12 h-12 bg-gray-700 rounded-full overflow-hidden shadow-inner text-center table">
+                        <span class="table-cell text-sm text-white font-bold align-middle">MT</span>
+                    </div>
+                    <div class="flex flex-col">
+                        <h3 class="whitespace-normal px-4 text-white">Toto Kelapile</h3>
+                        <h3 class="whitespace-normal px-4 text-xs text-gray-200">Toto Pty Ltd</h3>
+                        <h3 class="whitespace-normal px-4 text-xs text-gray-300">toto@gmail.com</h3>
+                    </div>
+                </div>
+                <a v-if="isAuth" href="/settings" v-bind:class="currentUrl == 'settings' ? 'text-white bg-gray-900 hover:text-gray-400':'text-gray-400 hover:text-white hover:bg-gray-900'" class="rounded my-navbar-link-mobile">
+                    <div class="text-sm font-bold">
+                        Settings
+                    </div>
+                </a>   
+                <a v-if="isAuth" href="/logout" v-on:click.prevent="logout" class="block mt-1 px-2 py-2 font-bold text-sm hover:no-underline rounded text-gray-400 hover:text-white hover:bg-gray-900 border-transparent focus:ring-offset-0 focus:ring-gray-300">Logout</a>
+            </div>
+            <!----Un-Authenticated----->
+            <a v-if="!isAuth" href="/login" class="block text-white rounded border border-white px-2 py-1 hover:no-underline hover:bg-gray-400 border-transparent focus:ring-offset-0 focus:ring-gray-300">Login</a>
+            <a v-if="!isAuth" href="/register" class="block mt-1 text-white rounded border border-white px-2 py-1 hover:no-underline hover:bg-gray-400 focus:ring-offset-0 focus:ring-gray-300">Register</a>
         </div>
 
         <!----->
@@ -95,6 +138,7 @@
 </template>
 
 <script>
+    import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
     export default {
         data () {
             return {
@@ -129,5 +173,11 @@
                 this.$refs.mform.submit();
             },
         },
+        components:{
+            Menu, 
+            MenuItem,
+            MenuItems,
+            MenuButton,
+        }
     }
 </script>
