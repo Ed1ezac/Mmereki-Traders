2889c4be8fae9d800e4ae7c0c92078e737944d38
Ed1ezac <ed1ezac@gmail.com>
2023-06-15 11:33:49 +0200

    adds terms and privacy policy

--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -1,5 +1,105 @@
 @extends('layouts.landing-header')
 
 @section('content')
-<div></div>
+<section class="lg:max-w-7xl lg:mx-auto px-4 sm:px-8 pb-8">
+    <div>
+        <h3 class="text-xl font-semibold text-gray-900">Privacy Policy</h3>
+        <hr/>
+        <p class="mt-2">
+            At Mmereki we believe in protecting our users’ personal data against prying eyes that may use 
+            it for foul means. That is why we have a clear policy on how we collect and use data for 
+            anyone who has any questions and queries to refer to.
+        </p>
+        <p class="mt-2">
+            This Privacy Policy describes the type of information we collect from you, why we collect that 
+            information and how we use it to deliver our services to you. You can contact us for more 
+            information regarding this Privacy Policy.
+        </p>
+    </div>
+    <ol class="mt-3 mb-6 pt-3" type="1">
+        <!--- Info Collected --->
+        <li><strong class="text-xl">1.What Information do we collect from you?</strong>
+            <br>
+            When you register an account with us, we ask you for the following information:
+            <ul class="ml-3" style="list-style-type:disc;">
+                <li>Your full names</li>
+                <li>A valid email address</li>
+                <li>Your contact number</li>
+                <li>A password</li>
+            </ul>
+            <p class="mt-2">
+                Note that this information (Your names and email address) may be attained from other 
+                third-party providers like Facebook, Twitter or Google when you choose to login with 
+                them. By doing so you authorize us to collect your information, such as your username, 
+                encrypted access credentials, and other information, that may be available on or 
+                through your social media account. This information includes but is not limited to 
+                your name, profile picture, country, e-mail address, date of birth and gender.
+            </p>
+            <p class="mt-2">
+                The email address and password you provide are used for authentication purposes. Your 
+                password is stored as an encrypted string in our databases and will never be known or 
+                determined by anyone (including our technicians and system administrators) other than 
+                you. Your email address is also used in case you forget your user password to send you 
+                a password reset link and recover your account.
+            </p>
+            <p class="mt-2">
+                Your email address or your contact number may be used as a contact detail to the Trade 
+                whom you accept to do your job. All other trades who show interest in a job you post on 
+                our website will never see your contact details. Even if you accepted a Trade to do a 
+                job previously, you will remain anonymous to them when you post a new job for bidding. 
+            </p>
+            <p class="mt-2">
+                When you leave a review for a Trade on a job they did for you, we ask you to provide us 
+                with the following: 
+            </p>
+            <ul class="ml-3" style="list-style-type:disc;">
+                <li>Your name; full or otherwise</li>
+                <li>Your email address</li>
+                <li>Contact number: either a cell number or a telephone number or both</li>
+            </ul>
+            <p class="mt-2">
+                We use this information to contact users about their reviews and to make sure they are 
+                genuine.
+            </p>
+            <p class="mt-2">
+                All reviews are posted on <a href="/" class="my-link">https://mmereki.co.bw</a>. The only 
+                information visible on the review post is your name, rating and comment. Your contact 
+                details will never be disclosed to any Trade regarding your review.
+            </p>
+        </li>
+
+        <!--- Info Usage --->
+        <li class="mt-6"><strong class="text-xl">2.How do we use this information?</strong>
+            <br>
+            We may pass your information to a limited number of third parties for the following reasons: 
+            <ul class="ml-3" style="list-style-type:disc;">
+                <li>To deliver the services you’ve asked for including passing details to payment providers.</li>
+                <li>For legal or regulatory purposes including fraud prevention.</li>
+                <li>In the event of a company merger or complete sale of part or all of our assets.</li>
+            </ul>
+            <p class="mt-2">
+                If you have submitted feedback on a Trade, we will keep a record of your personal information 
+                for as long as the review remains on the website. Keeping a record of all reviews allows 
+                us to monitor feedback and combat fraudulent activity
+            </p>
+            <p class="mt-2">
+                When a Trade is accepted for a job by a consumer, we share the Trade’s business information 
+                (name, full address, email address and phone number) to fulfil our contractual obligations 
+                to the consumer. In the event of a dispute between a consumer and a Trade, we will make 
+                available any other personal information we hold on a Trade’s Business that is pertinent 
+                to that dispute and may, in our judgement assist in the fair and reasonable resolution 
+                of that dispute.
+            </p>
+        </li>
+    </ol>
+    <p class="mt-2">
+        At Mmereki we work to protect the security of your information during transmission by using 
+        Secure Sockets Layer (SSL) software, which encrypts information you input in any of our 
+        website forms or fields. If, at any time, you feel that we haven’t processed your data 
+        fairly or you’re not satisfied with how we’ve handled your personal information, you can 
+        contact us with regard to this.
+    </p>
+</section>
+
+@include('components.footer-large')
 @endsection
--- resources/views/legal/terms.blade.php
+++ resources/views/legal/terms.blade.php
@@ -1,5 +1,282 @@
 @extends('layouts.landing-header')
 
 @section('content')
-<div></div>
+<section class="lg:max-w-7xl lg:mx-auto px-4 sm:px-8 pb-8">
+    <div>
+        <h3 class="text-xl font-semibold text-gray-900">Terms of Service</h3>
+        <hr/>
+        <p class="mt-2">Mmereki is a registered online application located in Gaborone, Botswana.</p>
+    </div>
+    <div class="mt-6 ml-4">
+        <ul style="list-style-type:disc;">
+            <li>Where we refer to ourselves (“Mmereki”) in this Agreement is also taken to 
+                include (where the context allows) our affiliates, our employees and associated persons.
+            </li>
+            <li>Where we refer to our members, we mean the Trades who have an account on the Website.</li>
+            <li>Where we refer to you in these Terms of Use, also includes any person that accesses or 
+                uses our Service on your behalf.
+            </li>
+            <li>The “Services” consists of the Website currently located at <a class="my-link" href="/">https://mmereki.co.bw"</a>
+                , any pages we operate on third party social media applications, and the content and 
+                services we make available through them via the internet, mobile devices including smart phones and tablets.
+            </li>
+        </ul>
+    </div>
+
+    <ol class="mt-3 mb-6 pt-3" type="1">
+        <!-- Disclaimer -->
+        <li class="mt-3"><strong class="text-xl">1.Disclaimer</strong>
+            <br/>
+            <ol type="i">
+                <li>Whilst every effort is made to verify statements and information submitted by the public 
+                    and Mmereki members, the information compiled by Mmereki is based on information supplied 
+                    by various individuals and businesses, it does not represent the views of Mmereki and 
+                    Mmereki cannot be held responsible for any omissions or inaccurate claims that may appear.
+                </li>
+                <li>
+                    Users of the service do hereby represent, understand and expressly agree that Mmereki does 
+                    not make any warranty or guarantee over the quality, timing, integrity, responsibility, 
+                    legality or any other aspect whatsoever of the materials or services offered or delivered 
+                    by its members to their customers.
+                </li>
+                <li>
+                    Mmereki expressly disclaims any liability whatsoever for any damage, suits, claims, and/or 
+                    controversies that have arisen or may arise, whether known or unknown therefrom.
+                </li>
+                <li>
+                    The Website services are provided on an "AS IS" and "AS AVAILABLE" basis without any 
+                    representation or endorsement made and without warranty of any kind whether express or implied, 
+                    including but not limited to the implied warranties of satisfactory quality, fitness for a 
+                    particular purpose, non-infringement, compatibility, security and accuracy. Use of the 
+                    service is at your own risk.
+                </li>
+                <li>
+                    To the extent permitted by law, Mmereki will not be liable for any indirect or consequential 
+                    loss or damage whatsoever (including without limitation loss of business, opportunity, data, 
+                    profits) arising out of or in connection with the use of the Website/Services.
+                </li>
+            </ol>
+        </li>
+
+        <!-- Acceptance of terms -->
+        <li class="mt-3 pt-3"><strong class="text-xl">2.Acceptance of Terms</strong>
+            <br/>
+            By accepting the terms of use you agree to the following:
+            <ol type="i">
+                <li>
+                    Your access to and use of <a class="my-link" href="/">https://mmereki.co.bw</a> and its 
+                    Services are subject exclusively to these Terms and Conditions.
+                </li>
+                <li>
+                    You agree not to use the Website and/or Services for any purpose that is unlawful or 
+                    prohibited by these Terms and Conditions.
+                </li>
+                <li>
+                    You agree to comply with all applicable laws regarding the transmission of technical 
+                    data exported from the Republic of Botswana or the country in which you reside 
+                    (if different) and with all local laws and rules regarding acceptable use of and 
+                    conduct on the Internet.
+                </li>
+                <li>
+                    You agree to indemnify and hold Mmereki and its employees and agents harmless from 
+                    and against any breach by you of these Terms and Conditions and any claim or demand 
+                    brought against Mmereki by any third party arising out of your use of the Services 
+                    and/or any Content submitted, posted or transmitted through the Services, including 
+                    without limitation, all claims, actions, proceedings, losses, liabilities, damages, 
+                    costs, expenses (including reasonable legal costs and expenses) howsoever suffered 
+                    or incurred by Mmereki in consequence of your breach of these Terms and Conditions.
+                </li>
+                <li>
+                    You hereby expressly agree not to hold Mmereki (or Mmereki agents, representatives, 
+                    advertisers or employees) liable for any instruction, advice or services delivered 
+                    which originated through the website or associated Services.
+                </li>
+                <li>
+                    If you are a member these Terms and Conditions shall apply to you as well as other 
+                    Terms and Conditions for members. In the event of a conflict between these Terms and 
+                    Conditions and the Terms and Conditions for members, the contents of those additional 
+                    terms shall prevail.
+                </li>
+                <li>
+                    You agree to use the Website for lawful purposes only and are subject to prosecution 
+                    if you use this website:
+                    <ol type="a">
+                        <li>To violate the privacy of others.</li>
+                        <li>To undermine the purpose and provision of its Services.</li>
+                        <li>To act in competition with its Services.</li>
+                        <li>To harm or attempting to harm any person, business or entity in anyway.</li>
+                        <li>To reverse engineer, decompile, disassemble, decipher or otherwise derive the code used on the Website.</li>
+                        <li>To use any robot, spider, scraper, or other automated means to access the Website.</li>
+                        <li>
+                            To reproduce, duplicate, copy, sub-licence, broadcast, distribute, sell, 
+                            re-brand or re-sell any part of the Website (without Mmereki’s express consent).
+                        </li>
+                        <li>
+                            In a way that is unlawful or fraudulent or has any unlawful or fraudulent purpose or effect.
+                        </li>
+                        <li>
+                            In any way that breaches any applicable local, national or international law or regulation.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </li>
+
+        <!-- Our services -->
+        <li class="mt-3"><strong>3.Our Services</strong>
+            <ol type="i">
+                <li>
+                    Mmereki serves as a middleman between Trades and Consumers, as such, the content 
+                    contained on the Website is provided for general information only. It is not intended 
+                    to amount to advice on which you should rely.
+                </li>
+                <li>
+                    The pricing offered by the Trade is not offered, underwritten or approved by Mmereki. 
+                    Any agreement reached between you and a Trade based on this or any other pricing 
+                    structure is a contract between you and that Trade and accordingly Mmereki is not a 
+                    party to it.
+                </li>
+                <li>
+                    Although Mmereki makes reasonable efforts to update the information on the Website, 
+                    please note that the content contained on the Website may be out of date at any given 
+                    time and Mmereki are under no obligation to update it.
+                </li>
+                <li>
+                    The Website/Services may include links to third party websites that are controlled 
+                    and maintained by others. Any link to other websites is not an endorsement of such 
+                    websites and you acknowledge and agree that Mmereki has no control over such websites 
+                    and is not responsible for the content or availability of any such websites.
+                </li>
+            </ol>
+        </li>
+
+        <!-- Our Rights -->
+        <li class="mt-3"><strong>Our Rights</strong>
+            <ol type="i">
+                <li>Mmereki reserves the right to temporarily or permanently withdraw your access to the Website.</li>
+                <li>
+                    Mmereki reserves the right to commence legal proceedings against you for your actions 
+                    regarding the unlawful use of the Website which is in breach of these Terms of Service.
+                </li>
+                <li>
+                    Mmereki reserves the right to disclose necessary information to law enforcement authorities
+                </li>
+                <li>
+                    Mmereki reserves the right to remove or withhold any content at any time which it considers 
+                    fails to adhere with the Terms of Service or Privacy Policy.
+                </li>
+                <li>
+                    Mmereki reserves the right to edit content you upload to amend spelling, grammar, punctuation, 
+                    length, for the purposes of clarity and to ensure that the content can comply with the Terms of 
+                    Service or Privacy Policy.
+                </li>
+                <li>
+                    Mmereki reserves the right to update or amend these Terms and Conditions at any time and 
+                    your continued use of the Website and/or Services following any changes shall be deemed to be 
+                    your acceptance of such change. It is therefore your responsibility to check the Terms and 
+                    Conditions regularly for any changes. This includes the Mmereki Privacy Policy which sets out 
+                    the terms on which we process any personal data we collect from you or that you provide to 
+                    Mmereki (“Privacy Policy”). The Privacy Policy also sets out who we share the content you 
+                    upload or provide with.
+                </li>
+            </ol>
+        </li>
+
+        <!-- Intellectual Property Rights -->
+        <li class="mt-3"><strong>Intellectual Property Rights</strong>
+            <ol type="i">
+                <li>
+                    When you upload any content (including text or photographs) to the Website, you 
+                    assign all rights in such content to Mmereki.
+                </li>
+                <li>
+                    You agree that once you have submitted content, the content will become the property 
+                    of Mmereki and you cannot recall any part of such content.
+                </li>
+                <li>
+                    None of the content contained on the Website may be downloaded, copied, reproduced, 
+                    transmitted, stored, sold or distributed without the prior written consent of Mmereki. 
+                    This excludes the downloading, copying and/or printing of pages of the Website for 
+                    personal, non-commercial use only or for use by Mmereki members in promoting their business.
+                </li>
+                <li>
+                    The Website and its content (including without limitation the Website design, text, 
+                    graphics and all software and source codes connected with the Website and the Services) 
+                    are protected by copyright, trademarks, and other intellectual property rights and laws. 
+                    In accessing the Website, you agree that you will access the contents solely for your 
+                    personal, non-commercial use.
+                </li>
+                
+                <p class="my-1">In regard to the above rights, when uploading content to the Website you must ensure that all content uploaded:</p>
+
+                <ol type="a">
+                <li>Is accurate, true, up to date, genuine and not misleading</li>
+                    <li>Is relevant and appropriate</li>
+                    <li>Complies with applicable law of the Republic of Botswana and in any country from which the content is posted or viewed and does not promote illegal activity</li>
+                    <li>Complies with the Privacy Policy and does not disclose any personal or confidential information</li>
+                    <li>Does not contain material which is or potentially could be defamatory, obscene, aggressive, offensive, hateful, inflammatory or rude</li>
+                    <li>Does not infringe the copyright or any intellectual property rights of any person and where such content relates to an image, you must ensure that all necessary consents and authorisations have been obtained.</li>
+                    <li>Does not promote discrimination based on race, sex, religion, nationality, disability, sexual orientation or age</li>
+                    <li>Does not relate to the services provided by Mmereki</li>
+                    <li>Is not threatening, abusive or invades others privacy or causes annoyance, inconvenience or needless anxiety</li>
+                    <li>Does not impersonate any person or misrepresent identities or affiliations</li>
+                </ol>
+            </ol>
+        </li>
+
+        <!-- Limitations -->
+        <li class="mt-3"><strong>Reliability Limitations and Liability</strong>
+            <ol type="i">
+                <li>
+                    Mmereki does not guarantee that the Website or any content contained on the Website will always be available or be uninterrupted.
+                </li>
+                <li>
+                    Mmereki may suspend, withdraw, discontinue or change all or any part of the Website without notice to you.
+                </li>
+                <li>Mmereki will not be liable to you for any reason if the Website is unavailable at any time or for any period.</li>
+                <li>
+                    Although Mmereki takes steps to verify the credentials and company status of its 
+                    members, you are responsible for making all arrangements necessary for you to verify this information.
+                </li>
+                <li>
+                    Mmereki aims to aid customers to the best of our ability. However, Mmereki cannot 
+                    guarantee a specific response time and will not be liable for the failure of any 
+                    member to succeed in providing any service in any specific time quoted.
+                </li>
+                <li>
+                    Mmereki cannot guarantee that the Website will be secure or free from bugs or viruses.
+                </li>
+                <li>
+                    Mmereki will not be liable for any loss or damage caused by a virus, or any other 
+                    technologically harmful material that may infect your computer equipment, data, 
+                    computer programs or other proprietary material due to your use of the Website or 
+                    to your downloading of any content on the Website, or linked to the Website, where 
+                    Mmereki has taken reasonable security to prevent this.
+                </li>
+            </ol>
+        </li>
+
+        <!-- Governing Law -->
+        <li class="mt-3"><strong>Governing Law</strong>
+            <ol type="i">
+                <li>
+                    These Terms and Conditions shall be governed by and construed in accordance with 
+                    the law of the Republic of Botswana and you hereby submit to the exclusive jurisdiction 
+                    to the courts of Botswana.
+                </li>
+                <li>
+                    If any of these Terms and Conditions should be determined to be invalid, illegal 
+                    or unenforceable for any reason by any court of competent jurisdiction then such 
+                    Term or Condition shall be severed and the remaining Terms and Conditions shall 
+                    survive and remain in full force and effect and continue to be binding and enforceable.
+                </li>
+            </ol>
+        </li>
+    </ol>
+
+    <hr/> 
+    <small class="py-2">Revised on:&nbsp;&nbsp;&nbsp;&nbsp;14 June 2023</small>
+</section>
+
+@include('components.footer-large')
 @endsection
