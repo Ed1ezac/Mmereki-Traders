2a7030a87ff0555e6cabc30f1022dcd1512dba1b
Ed1ezac <ed1ezac@gmail.com>
2023-07-09 18:12:09 +0200

    adds minor edits

--- resources/views/layouts/landing-header.blade.php
+++ resources/views/layouts/landing-header.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{{ config('app.name', 'Mmereki-Traders') }}</title>
+        <title>{{ config('app.name', 'Mmereki-Tradesmen') }}</title>
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         
